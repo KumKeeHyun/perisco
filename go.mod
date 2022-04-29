f69e2dd71d11210ec7ab0498f4cf033e5f786cd3
KumKeeHyun <kbzjung359@gmail.com>
2022-04-29 12:10:17 +0000

    init perisco for bpf

--- go.mod
+++ go.mod
@@ -2,6 +2,6 @@ module github.com/KumKeeHyun/perisco
 
 go 1.17
 
-require (
-    github.com/cilium/ebpf v0.8.2
-)
+require github.com/cilium/ebpf v0.8.2-0.20220217141816-62da0a730ab7
+
+require golang.org/x/sys v0.0.0-20210906170528-6f6e22806c34 // indirect
--- perisco/main.go
+++ perisco/main.go
@@ -1,7 +1,15 @@
 package main
 
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc $BPF_CLANG -cflags $BPF_CFLAGS -target bpfel -type event bpf $BPF_FILES -- -I$BPF_HEADER
+import (
+	"log"
+
+	"github.com/cilium/ebpf/rlimit"
+)
+
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc $BPF_CLANG -cflags $BPF_CFLAGS -target bpfel -type event bpf $BPF_FILES -- -I$BPF_HEADERS
 
 func main() {
-	
+	if err := rlimit.RemoveMemlock(); err != nil {
+		log.Fatal(err)
+	}
 }

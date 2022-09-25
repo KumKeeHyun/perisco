086e171e457421f82585c6fc3da8a6655d90a699
KumKeeHyun <kbzjung359@gmail.com>
2022-09-25 04:34:34 +0000

    update install-protoc script

--- perisco/testing/grpc/hello/install-protoc.sh
+++ perisco/testing/grpc/hello/install-protoc.sh
@@ -8,3 +8,6 @@ rm -rf protoc.zip
 
 go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.28
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2
+
+echo "export PATH=$PATH:$(go env GOPATH)/bin" >> ~/.bashrc
+source ~/.bashrc

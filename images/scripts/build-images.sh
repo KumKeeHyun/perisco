76fb551cb49f08d7b1ee72d6d08171b4749dae25
KumKeeHyun <kbzjung359@gmail.com>
2023-10-09 12:45:13 +0000

    add perisco builder images, modify perisco Dockerfile

--- images/scripts/build-images.sh
+++ images/scripts/build-images.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
 root_dir="$(git rev-parse --show-toplevel)"
-perisco_dir="${root_dir}/perisco"
 perisco_dockerfile="${root_dir}/images/perisco/Dockerfile"
-
-cd "${perisco_dir}"
-make bpf
 
 cd "${root_dir}"
 docker build -f "${perisco_dockerfile}" -t kbzjung359/perisco:0.0.2 .
